9b39eb55be550f3b2d30d8d21168138e52dd983c
Sample User <sample@example.com>
2024-07-07 23:59:55 +0000

    Scripts solicitados Item 1

---
+++ integrantes.py
@@ -0,0 +1,10 @@
+# integrantes.py
+
+integrantes = [
+    "Jorge Meza",
+    "Max Aguilera",
+]
+
+print ("integrantes grupo examen transversal:")
+for integrante in integrantes:
+    print(integrante)

441613809caeb5e2799568baa98ce77700b8b564
Sample User <sample@example.com>
2024-07-10 23:26:46 +0000

    Item3 DBbrowser

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey12345'  # Cambia esto por una clave secreta más segura
+app.config['SECRET_KEY'] = 'secretkey123456'  # Cambia esto por una clave secreta más segura
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
